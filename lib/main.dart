00d008d843958a2025064b6c0e584cda5a3e58fa
BullsEye <18664297+BullsEye34@users.noreply.github.com>
2024-12-09 21:45:08 +0530

    - Implement Hover animation - Implement Drag and drop

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Dock(
@@ -22,16 +23,26 @@ class MyApp extends StatelessWidget {
               Icons.camera,
               Icons.photo,
             ],
-            builder: (e) {
-              return Container(
+            builder: (icon, isHovered) {
+              return AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
                 constraints: const BoxConstraints(minWidth: 48),
-                height: 48,
+                height: isHovered ? 64 : 48,
+                width: isHovered ? 64 : 48,
                 margin: const EdgeInsets.all(8),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(8),
-                  color: Colors.primaries[e.hashCode % Colors.primaries.length],
+                  color: Colors.primaries[icon.hashCode % Colors.primaries.length],
+                  boxShadow: isHovered
+                      ? [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            blurRadius: 10,
+                          ),
+                        ]
+                      : [],
                 ),
-                child: Center(child: Icon(e, color: Colors.white)),
+                child: Center(child: Icon(icon, color: Colors.white)),
               );
             },
           ),
@@ -42,39 +53,74 @@ class MyApp extends StatelessWidget {
 }
 
 /// Dock of the reorderable [items].
-class Dock<T> extends StatefulWidget {
+class Dock extends StatefulWidget {
   const Dock({
     super.key,
     this.items = const [],
     required this.builder,
   });
 
-  /// Initial [T] items to put in this [Dock].
-  final List<T> items;
+  /// Initial [IconData] items to put in this [Dock].
+  final List<IconData> items;
 
-  /// Builder building the provided [T] item.
-  final Widget Function(T) builder;
+  /// Builder building the provided [IconData] item with a hover effect.
+  final Widget Function(IconData, bool isHovered) builder;
 
   @override
-  State<Dock<T>> createState() => _DockState<T>();
+  State<Dock> createState() => _DockState();
 }
 
 /// State of the [Dock] used to manipulate the [_items].
-class _DockState<T> extends State<Dock<T>> {
-  /// [T] items being manipulated.
-  late final List<T> _items = widget.items.toList();
+class _DockState extends State<Dock> {
+  /// [IconData] items being manipulated.
+  late final List<IconData> _items = widget.items.toList();
+
+  /// Currently hovered index.
+  int? _hoveredIndex;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
+        borderRadius: BorderRadius.circular(16),
         color: Colors.black12,
       ),
-      padding: const EdgeInsets.all(4),
+
+      /// Specify height for a smooth transition when hovering
+      /// 64 is the max height of Icon when hovered
+      /// So, 64 plus padding where equals to 96
+      height: 96.0,
+      padding: const EdgeInsets.all(8),
       child: Row(
         mainAxisSize: MainAxisSize.min,
-        children: _items.map(widget.builder).toList(),
+        children: List.generate(_items.length, (index) {
+          return DragTarget<IconData>(
+            onWillAcceptWithDetails: (data) => data.data != _items[index],
+            onAcceptWithDetails: (data) {
+              setState(() {
+                final oldIndex = _items.indexOf(data.data);
+                final newIndex = index;
+                _items.removeAt(oldIndex);
+                _items.insert(newIndex, data.data);
+              });
+            },
+            builder: (context, candidateData, rejectedData) {
+              return Draggable<IconData>(
+                data: _items[index],
+                feedback: Material(
+                  color: Colors.transparent,
+                  child: widget.builder(_items[index], true),
+                ),
+                childWhenDragging: const SizedBox.shrink(),
+                child: MouseRegion(
+                  onEnter: (_) => setState(() => _hoveredIndex = index),
+                  onExit: (_) => setState(() => _hoveredIndex = null),
+                  child: widget.builder(_items[index], _hoveredIndex == index),
+                ),
+              );
+            },
+          );
+        }),
       ),
     );
   }
